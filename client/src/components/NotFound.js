ad58e946f1b17f62c721ae66ec1e22f04daa8284
TahaDriss <taha.driss@esprit.tn>
2019-03-28 13:30:14 +0100

    React routing : dashboard + notFound

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-4">404 page not found</h1>
+        <p className="lead">sorry, the page does not exist</p>
+      </div>
+    );
+  }
+}
