75adbc4202c268992aa827758728d52beb925eb4
Ahmed Ben Ghozzia <ahmed.benghozzia@esprit.tn>
2019-04-18 21:46:06 +0100

    Machine Learning

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import Setting from "./components/Settings";
-import SideBar from "./components/SideBar";
 
 import store from './store';
 import { loadUser } from './actions/authActions'
@@ -17,6 +15,9 @@ import NotFound from "./components/NotFound";
 import Register from './components/auth/Register';
 import { Provider } from 'react-redux';
 import Login from './components/auth/Login';
+import Setting from "./components/Settings";
+import SideBar from "./components/SideBar";
+import Dashboard from "./components/Dashboard";
 
 class App extends Component {
 
@@ -30,9 +31,30 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/">
+              <div className="container-scroller">
+                <NavBar />
+                <div className="container-fluid page-body-wrapper">
+                  <Setting />
+                  <SideBar />
+                  <div className="main-panel">
+                    <div className="content-wrapper">
+                      <Router>
+                        <Switch>
+                          <Route exact path="/" component={Dashboard} />
+                          <Route
+                            path="/contract/:address"
+                            component={CatalystDetails}
+                          />
+                        </Switch>
+                      </Router>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Route>
             <Route path="/register" component={Register} />
-            <Route path="/login" component={Login}/>
+            <Route path="/login" component={Login} />
             <Route component={NotFound} />
           </Switch>
         </Router>
--- routes/api/PredictRoute.js
+++ routes/api/PredictRoute.js
@@ -11,59 +11,59 @@ var content1 = fs.readFileSync('data/predict.json');
 var predictData = JSON.parse(content1);
 
 const trainData = tf.tensor2d(weather.map(item => [
+  [
+    item.Hour,
     item.Month,
     item.Day,
-    item.Hour,
-    item.temperature,
-    item.cellTemperature,
-    item.windSpeed
+    item.Year
+  ],[
+    item["Cloud Coverage"],
+    item.Visibility,
+    item.Temperature,
+    item["Dew Point"],
+    item["Wind Speed"]
+  ]
 ]))
 
+console.log(trainData)
+
 const outputData = tf.tensor2d(weather.map(item => [
-    item.energy
+    item.Energy
 ]))
 
 const testingData = tf.tensor2d(predictData.map(item => [
-    item.Month,
-    item.Day,
-    item.Hour,
-    item.temperature,
-    item.cellTemperature,
-    item.windSpeed
+    item["Cloud Coverage"],
+    item.Visibility,
+    item.Temperature,
+    item["Dew Point"],
+    item["Wind Speed"]
 ]))
 
 const model = tf.sequential()
 
 model.add(tf.layers.dense({
-    inputShape: [6],
-    activation: "sigmoid",
+    inputShape: [5],
+    activation: "linear",
     units: 64
-}))
-
-model.add(tf.layers.dense({
+  }))
+  
+  model.add(tf.layers.dense({
     inputShape: [64],
-    activation: "sigmoid",
-    units: 64
-}))
-
-model.add(tf.layers.dense({
-    inputShape: [64],
-    activation: "tanh",
-    units: 64
-}))
-
-
-model.add(tf.layers.dense({
-    activation: "relu",
+    activation: "linear",
     units: 1
-}))
-
+  }))
+  
+  
+  model.add(tf.layers.dense({
+    activation: "linear",
+    units: 1
+  }))
 model.compile({
     loss: "meanSquaredError",
     optimizer: tf.train.adam(.06),
 })
 
-model.fit(trainData, outputData, { epochs: 100}).then((history) => console.log(history))
+model.fit(trainData, outputData, { epochs: 100, shuffle: true}).then((history) => console.log(history))
 
 router.get('/', function (req, res, next) {
     res.send(model.predict(testingData).dataSync())
