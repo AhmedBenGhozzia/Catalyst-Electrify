1f2183cae65f4830dcb3ab18f3cf31aa14339240
TahaDriss <taha.driss@esprit.tn>
2019-05-05 15:49:42 +0100

    design updates

--- client/src/ethereum/factory.js
+++ client/src/ethereum/factory.js
@@ -5,7 +5,7 @@ const instance = new web3.eth.Contract(
   JSON.parse(CatalystFactory.interface),
   //"0xB3d63417c293A4b175f4217FC8B20E0dcD774DD6"  //Rinkeby
   "0x3e7be99ebceafd71a4d0a903967c53d29b830041" //Local private network
-  //"0xe49f39e2383193d42ccc15404a51fd4d4fe0d0de" //AWS private network
+  // "0xe49f39e2383193d42ccc15404a51fd4d4fe0d0de" //AWS private network
 );
 
 export default instance;
